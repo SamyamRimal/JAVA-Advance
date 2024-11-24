5be9b660ca328a8870759e91a8930eb4c6aa74ed
SamyamRimal <157302997+SamyamRimal@users.noreply.github.com>
2024-11-24 20:18:45 +0545

    Add files via upload

---
+++ sorting.java
@@ -0,0 +1,22 @@
+public class sorting {
+    public static void assending(int[] arr){
+        for(int i=0;i<arr.length-1;i++){
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] arr ={64, 34, 25, 12, 22, 11, 90};
+
+        for(int i = 0; i < arr.length-1; i++) {
+            for(int j = 0; j < arr.length-i-1; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        assending(arr);
+    }
+}
